ce9edd02f273074f88bddec2588d7280bdb3efb9
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-08 18:58:45 +0200

    Change texture dimensions to use thumbnail sizes
    Updated texture width and height calculations to use thumbnail dimensions.

--- src/Flip3DComp_View.cpp
+++ src/Flip3DComp_View.cpp
@@ -143,8 +143,11 @@ HWND Flip3DCompApp::HitTest3DScene(LONG screenX, LONG screenY) const
         const float flatRank = ComputeFlatDepthRank(slot, p, ki);
         auto  MVP = Math::Multiply(BuildModelMatrix(c, t, p, flatRank), cam);
 
-        float sw = (float)std::max(c.m_srcWidth,  1);
-        float sh = (float)std::max(c.m_srcHeight, 1);
+        //float sw = (float)std::max(c.m_srcWidth,  1);
+        //float sh = (float)std::max(c.m_srcHeight, 1);
+        float sw = (float)std::max(c.m_thumbTexWidth,  1);
+        float sh = (float)std::max(c.m_thumbTexHeight, 1);
+        
 
         auto project = [&](float px, float py) -> Vec2
         {
