c1b3977d85c1513524c094027532e6ed11ce742f
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-09 22:10:43 +0200

    Update CardModel.h

--- src/CardModel.h
+++ src/CardModel.h
@@ -42,6 +42,10 @@ struct CardModel
     float               m_occupancy    = 0.7f;       // normalized occupancy factor
     int                 m_srcWidth     = 400;        // thumbnail source width (pixels)
     int                 m_srcHeight    = 300;        // thumbnail source height (pixels)
+
+    int                 m_nativeSrcWidth  = 400;     // queried DWM source width (pixels)
+    int                 m_nativeSrcHeight = 300;     // queried DWM source height (pixels)
+
     bool                m_isMinimized  = false;      // window is iconic
     bool                m_isShellDesktop = false;    // Progman / GetShellWindow()
 
