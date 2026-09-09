4a6631f4234b45a8278eac56a23523a9b2059c14
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-10 00:15:43 +0200

    Refactor CreateAppWindow with taskbar handling
    Uncommented the CreateAppWindow function and added taskbar visibility handling.

--- src/Flip3DComp_Window.cpp
+++ src/Flip3DComp_Window.cpp
@@ -78,6 +78,52 @@ void Flip3DCompApp::ApplyFullscreenLayout()
 // Flip3DCompApp::CreateAppWindow
 // uDWM Flip3D input window: borderless popup, topmost, full virtual desktop.
 // ============================================================================
+/*bool Flip3DCompApp::CreateAppWindow()
+{
+    WNDCLASSEXW wc = {
+        sizeof(wc),
+        CS_HREDRAW | CS_VREDRAW,
+        &Flip3DCompApp::WndProc,
+        0, 0,
+        m_hInstance,
+        nullptr,
+        LoadCursorW(nullptr, IDC_ARROW),
+        nullptr, nullptr,
+        L"Flip3DCompClass",
+        nullptr,
+    };
+    RegisterClassExW(&wc);
+
+    const int x = GetSystemMetrics(SM_XVIRTUALSCREEN);
+    const int y = GetSystemMetrics(SM_YVIRTUALSCREEN);
+    const int w = GetSystemMetrics(SM_CXVIRTUALSCREEN);
+    const int h = GetSystemMetrics(SM_CYVIRTUALSCREEN);
+
+    m_hwnd = CreateWindowExW(
+        WS_EX_NOREDIRECTIONBITMAP | WS_EX_TOPMOST | WS_EX_TOOLWINDOW,
+        L"Flip3DCompClass",
+        L"",
+        WS_POPUP,
+        x, y, w, h,
+        nullptr, nullptr,
+        m_hInstance,
+        this);
+
+    if (!m_hwnd)
+        return false;
+
+    m_rtl = (GetWindowLongPtrW(m_hwnd, GWL_EXSTYLE) & WS_EX_LAYOUTRTL) != 0;
+
+    RECT client = {};
+    if (GetClientRect(m_hwnd, &client))
+    {
+        m_width  = std::max(1u, (UINT)(client.right  - client.left));
+        m_height = std::max(1u, (UINT)(client.bottom - client.top));
+    }
+
+    return true;
+}*/
+
 bool Flip3DCompApp::CreateAppWindow()
 {
     WNDCLASSEXW wc = {
@@ -111,6 +157,26 @@ bool Flip3DCompApp::CreateAppWindow()
 
     if (!m_hwnd)
         return false;
+
+    APPBARDATA abd = {};
+    abd.cbSize = sizeof(APPBARDATA);
+    UINT appBarState = static_cast<UINT>(SHAppBarMessage(ABM_GETSTATE, &abd));
+    bool taskbarAutoHide = (appBarState & ABS_AUTOHIDE) != 0;
+
+    if (!taskbarAutoHide)
+    {
+        HWND hTaskbar = FindWindowW(L"Shell_TrayWnd", nullptr);
+        if (hTaskbar)
+        {
+            ShowWindow(hTaskbar, SW_SHOW);
+            HWND hSecondaryTray = FindWindowW(L"Shell_SecondaryTrayWnd", nullptr);
+            if (hSecondaryTray)
+            {
+                ShowWindow(hSecondaryTray, SW_SHOW);
+            }
+        }
+    }
+    // ---------------------------------------------------------------------
 
     m_rtl = (GetWindowLongPtrW(m_hwnd, GWL_EXSTYLE) & WS_EX_LAYOUTRTL) != 0;
 
