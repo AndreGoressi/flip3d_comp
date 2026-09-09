2c6f7008b4441707d81c4a4a9a7de4edeb4864a5
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-10 01:02:52 +0200

    Make taskbar and secondary tray visible and topmost
    Show and set taskbar and secondary tray windows to topmost.

--- src/Flip3DComp_Window.cpp
+++ src/Flip3DComp_Window.cpp
@@ -159,8 +159,23 @@ bool Flip3DCompApp::CreateAppWindow()
         m_hInstance,
         this);
 
+
     if (!m_hwnd)
         return false;
+
+    HWND hTaskbar = FindWindowW(L"Shell_TrayWnd", nullptr);
+    if (hTaskbar)
+    {
+        ShowWindow(hTaskbar, SW_SHOW);
+        SetWindowPos(hTaskbar, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_SHOWWINDOW);
+        
+        HWND hSecondaryTray = FindWindowW(L"Shell_SecondaryTrayWnd", nullptr);
+        if (hSecondaryTray)
+        {
+            ShowWindow(hSecondaryTray, SW_SHOW);
+            SetWindowPos(hSecondaryTray, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_SHOWWINDOW);
+        }
+    }
 
     m_rtl = (GetWindowLongPtrW(m_hwnd, GWL_EXSTYLE) & WS_EX_LAYOUTRTL) != 0;
 
