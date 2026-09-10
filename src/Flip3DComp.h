f4e74f21db97e0a5a17694c570a9d728530013c2
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-10 04:38:35 +0200

    Remove unused methods from Flip3DComp.h
    Removed unused DumpBandsEnumProc and DumpWindowBands methods.

--- src/Flip3DComp.h
+++ src/Flip3DComp.h
@@ -79,16 +79,6 @@ private:
     bool CreateAppWindow();
     void    ApplyFullscreenLayout();
 
-
-
-    static BOOL CALLBACK DumpBandsEnumProc(
-    HWND hwnd,
-    LPARAM lParam);
-
-    void DumpWindowBands();
-
-
-
     // ========================================================================
     // DWM Thumbnail API (dwmapi.dll ordinals 147, 162)
     // ========================================================================
