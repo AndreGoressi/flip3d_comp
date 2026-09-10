26a873301b910acf905b6e2439ea9d8c6614390f
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-10 03:03:08 +0200

    Add files via upload

---
+++ src/WindowBand.cpp
@@ -0,0 +1,54 @@
+#include "WindowBand.h"
+
+static CreateWindowInBand_t g_CreateWindowInBand = nullptr;
+
+bool WindowBand::Initialize()
+{
+    if (g_CreateWindowInBand)
+        return true;
+
+    auto user32 = GetModuleHandleW(L"user32.dll");
+
+    if (!user32)
+        return false;
+
+    g_CreateWindowInBand =
+        reinterpret_cast<CreateWindowInBand_t>(
+            GetProcAddress(
+                user32,
+                "CreateWindowInBand"));
+
+    return g_CreateWindowInBand != nullptr;
+}
+
+HWND WindowBand::CreateBandWindow(
+    DWORD exStyle,
+    ATOM atom,
+    LPCWSTR title,
+    DWORD style,
+    int x,
+    int y,
+    int width,
+    int height,
+    HINSTANCE instance,
+    LPVOID param,
+    DWORD band)
+{
+    if (!Initialize())
+        return nullptr;
+
+    return g_CreateWindowInBand(
+        exStyle,
+        atom,
+        title,
+        style,
+        x,
+        y,
+        width,
+        height,
+        nullptr,
+        nullptr,
+        instance,
+        param,
+        band);
+}
---
+++ src/WindowBand.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "banding.h"
+
+namespace WindowBand
+{
+    bool Initialize();
+
+    HWND CreateBandWindow(
+        DWORD exStyle,
+        ATOM atom,
+        LPCWSTR title,
+        DWORD style,
+        int x,
+        int y,
+        int width,
+        int height,
+        HINSTANCE instance,
+        LPVOID param,
+        DWORD band);
+}
