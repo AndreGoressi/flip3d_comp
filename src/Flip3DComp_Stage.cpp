24c8d14a8590daa16397c45eeaabe117824c2074
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-11 19:25:48 +0200

    Implement InitializeDCompStage in Flip3DCompApp

--- src/Flip3DComp_Stage.cpp
+++ src/Flip3DComp_Stage.cpp
@@ -98,7 +98,7 @@ void Flip3DComp::ApplyFullscreenLayout()
 // Flip3DComp::CreateAppWindow
 // uDWM Flip3D input window: borderless popup, topmost, full virtual desktop.
 // ============================================================================
-bool Flip3DComp::InitializeDCompStage()
+/*bool Flip3DComp::InitializeDCompStage()
 {
     WNDCLASSEXW wc = {
         sizeof(wc),
@@ -145,6 +145,97 @@ bool Flip3DComp::InitializeDCompStage()
     //
     if (!m_hwnd)
         return false;
+
+    m_rtl = (GetWindowLongPtrW(m_hwnd, GWL_EXSTYLE) & WS_EX_LAYOUTRTL) != 0;
+
+    RECT client = {};
+    if (GetClientRect(m_hwnd, &client))
+    {
+        m_width  = std::max(1u, (UINT)(client.right  - client.left));
+        m_height = std::max(1u, (UINT)(client.bottom - client.top));
+    }
+
+    return true;
+}*/
+
+bool Flip3DCompApp::InitializeDCompStage()
+{
+    WNDCLASSEXW wc = {
+        sizeof(wc),
+        CS_HREDRAW | CS_VREDRAW,
+        &Flip3DCompApp::WndProc,
+        0, 0,
+        m_hInstance,
+        nullptr,
+        LoadCursorW(nullptr, IDC_ARROW),
+        nullptr, nullptr,
+        L"Flip3DCompClass",
+        nullptr,
+    };
+    ATOM atom = RegisterClassExW(&wc);
+    if (!atom)
+    {
+        return false;
+    }
+
+    int x = GetSystemMetrics(SM_XVIRTUALSCREEN);
+    int y = GetSystemMetrics(SM_YVIRTUALSCREEN);
+    int w = GetSystemMetrics(SM_CXVIRTUALSCREEN);
+    int h = GetSystemMetrics(SM_CYVIRTUALSCREEN);
+    //
+    HWND hTaskbar = FindWindowW(L"Shell_TrayWnd", nullptr);
+    if (hTaskbar && IsWindowVisible(hTaskbar))
+    {
+        RECT rcTaskbar = {};
+        if (GetWindowRect(hTaskbar, &rcTaskbar))
+        {
+            // bottom docked?
+            if (rcTaskbar.top <= y + h && rcTaskbar.bottom >= y + h)
+            {
+                h -= (rcTaskbar.bottom - rcTaskbar.top);
+            }
+            // top docked?
+            else if (rcTaskbar.top <= y && rcTaskbar.bottom >= y)
+            {
+                int tbHeight = rcTaskbar.bottom - rcTaskbar.top;
+                y += tbHeight;
+                h -= tbHeight;
+            }
+            // left docked?
+            else if (rcTaskbar.left <= x && rcTaskbar.right >= x)
+            {
+                int tbWidth = rcTaskbar.right - rcTaskbar.left;
+                x += tbWidth;
+                w -= tbWidth;
+            }
+            // right docked?
+            else if (rcTaskbar.left <= x + w && rcTaskbar.right >= x + w)
+            {
+                w -= (rcTaskbar.right - rcTaskbar.left);
+            }
+        }
+    }
+
+    m_hwnd = WindowBand::CreateWindowInBand(
+        WS_EX_NOREDIRECTIONBITMAP | WS_EX_TOOLWINDOW,
+        atom,
+        L"Flip3DCompClass",
+        WS_POPUP | WS_VISIBLE,
+        x,
+        y,
+        w,
+        h,
+        m_hInstance,
+        this,
+        ZBID_DESKTOP
+    );
+
+    if (!m_hwnd)
+        return false;
+
+    BOOL exclude = TRUE;
+    DwmSetWindowAttribute(m_hwnd, DWMWA_EXCLUDED_FROM_PEEK, &exclude, sizeof(exclude));
+    WindowCompositionAttribute::EnableAcrylic(m_hwnd);
 
     m_rtl = (GetWindowLongPtrW(m_hwnd, GWL_EXSTYLE) & WS_EX_LAYOUTRTL) != 0;
 
