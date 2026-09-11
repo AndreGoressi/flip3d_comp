2ebf5536e3f1db8f8b0f05f920789e2e63f9d9a6
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-11 18:25:52 +0200

    Update Flip3DComp.cpp

--- src/Flip3DComp.cpp
+++ src/Flip3DComp.cpp
@@ -49,13 +49,6 @@ bool Flip3DCompApp::Initialize(HINSTANCE hInstance)
             m_initError = L"Failed to create DWM thumbnail visuals.";
         return false;
     }
-
-    /*if (FAILED(CreateShellBackdrop()))
-    {
-        if (m_initError.empty())
-            m_initError = L"Failed to create the shell desktop backdrop.";
-        return false;
-    }*/
     
     m_state = ViewState::Enter;
     m_animEnter.Restart(0.0f, 1.0f, kEnterExitDurationSec);
