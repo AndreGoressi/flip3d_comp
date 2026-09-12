0f37777b615b70996da928dcb06672d6d9a7ff15
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-12 15:44:21 +0200

    Clean up commented code in Flip3DComp_View.cpp
    Removed commented-out code for shell window handling and a SendMessage call.

--- src/Flip3DComp_View.cpp
+++ src/Flip3DComp_View.cpp
@@ -77,12 +77,12 @@ void Flip3DComp::SelectWindow(HWND hwndTarget)
 
     const bool isShell = (hwndTarget == GetShellWindow());
 
-    /*if (isShell)
+    if (isShell)
     {
         if (HWND shellTray = FindWindowW(L"Shell_TrayWnd", nullptr))
             PostMessageW(shellTray, 0x579, 1, 0);
     }
-    else */if (!IsWindowEnabled(hwndTarget))
+    else if (!IsWindowEnabled(hwndTarget))
     {
         SwitchToThisWindow(GetLastActivePopup(GetAncestor(hwndTarget, GA_ROOTOWNER)), TRUE);
     }
@@ -101,7 +101,7 @@ void Flip3DComp::SelectWindow(HWND hwndTarget)
     if (m_cards[(size_t)selIdx].m_isMinimized)
     {
         ShowWindowAsync(hwndTarget, SW_SHOWNOACTIVATE);
-        SendMessage(hwndTarget, WM_SYSCOMMAND, SC_RESTORE, 0);
+        //SendMessage(hwndTarget, WM_SYSCOMMAND, SC_RESTORE, 0);
         //
         UpdateCardGeometry(m_cards[(size_t)selIdx], m_monW, m_monH, /*selectedRestore=*/true);
     }
