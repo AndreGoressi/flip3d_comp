f92125de861c6832ca9a37b24794d31192dc71dd
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-12 13:30:07 +0200

    Update Flip3DComp_ShellHook.cpp

--- src/Flip3DComp_ShellHook.cpp
+++ src/Flip3DComp_ShellHook.cpp
@@ -210,7 +210,7 @@ HRESULT Flip3DComp::CreateCardVisual(CardModel& card)
     ComPtr<IDCompositionRectangleClip> clip;
     if (SUCCEEDED(m_dcompDevice->CreateRectangleClip(&clip)))
     {
-        float radius = 5.5f;
+        float radius = 5.0f;
         clip->SetLeft(0.f);
         clip->SetTop(0.f);
         clip->SetRight((float)card.m_srcWidth);
