fc3b464461cea2913706d54c09dea972db4e4ec8
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-12 16:45:48 +0200

    Update Flip3DComp_View.cpp

--- src/Flip3DComp_View.cpp
+++ src/Flip3DComp_View.cpp
@@ -100,11 +100,13 @@ void Flip3DComp::SelectWindow(HWND hwndTarget)
 
     if (m_cards[(size_t)selIdx].m_isMinimized)
     {
-        ShowWindowAsync(hwndTarget, SW_SHOWNOACTIVATE);
-        //
-        UpdateCardGeometry(m_cards[(size_t)selIdx], m_monW, m_monH, /*selectedRestore=*/true);
+        //ShowWindowAsync(hwndTarget, SW_SHOWNOACTIVATE);
         //
         SendMessageW(hwndTarget, WM_SYSCOMMAND, SC_RESTORE, 0);
+        UpdateCardGeometry(m_cards[(size_t)selIdx], m_monW, m_monH, /*selectedRestore=*/true);
+        ShowWindowAsync(hwndTarget, SW_SHOWNOACTIVATE);
+        //
+        //SendMessageW(hwndTarget, WM_SYSCOMMAND, SC_RESTORE, 0);
     }
 
     m_selectedHwnd = hwndTarget;
