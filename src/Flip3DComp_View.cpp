85681b2eff94798ec2a44fe041d7334b2e5b2617
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-12 16:19:01 +0200

    Refactor UpdateCardGeometry call and clean comments
    Removed redundant comment and adjusted code formatting.

--- src/Flip3DComp_View.cpp
+++ src/Flip3DComp_View.cpp
@@ -100,10 +100,10 @@ void Flip3DComp::SelectWindow(HWND hwndTarget)
 
     if (m_cards[(size_t)selIdx].m_isMinimized)
     {
-        UpdateCardGeometry(m_cards[(size_t)selIdx], m_monW, m_monH, /*selectedRestore=*/true);
-        //
         ShowWindowAsync(hwndTarget, SW_SHOWNOACTIVATE);
         SendMessageW(hwndTarget, WM_SYSCOMMAND, SC_RESTORE, 0);
+        //
+        UpdateCardGeometry(m_cards[(size_t)selIdx], m_monW, m_monH, /*selectedRestore=*/true);
     }
 
     m_selectedHwnd = hwndTarget;
