2bcd6a3fc3922c1c7db5c7d9dc442a8423bbc142
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-12 13:46:25 +0200

    Show taskbar window when enabling blur effect

--- src/Flip3DComp_Stage.cpp
+++ src/Flip3DComp_Stage.cpp
@@ -140,6 +140,13 @@ bool Flip3DComp::InitializeDCompStage()
     BOOL exclude = TRUE;
     DwmSetWindowAttribute(m_hwnd, DWMWA_EXCLUDED_FROM_PEEK, &exclude, sizeof(exclude));
     WindowCompositionAttribute::EnableBlurBehind(m_hwnd);
+
+    HWND hTaskbar = FindWindowW(L"Shell_TrayWnd", nullptr);
+    if (hTaskbar)
+    {
+        SetWindowPos(hTaskbar, HWND_TOP, 0, 0, 0, 0, 
+                     SWP_NOMOVE | SWP_NOSIZE | SWP_SHOWWINDOW);
+    }
     //
     if (!m_hwnd)
         return false;
