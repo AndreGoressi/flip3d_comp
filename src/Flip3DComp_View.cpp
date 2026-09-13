8e2f9cb28a2ebd0781d1111dc9239cc7edff1dcc
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-13 13:51:49 +0200

    Disable window restore for minimized cards
    Comment out window restore commands for minimized cards.

--- src/Flip3DComp_View.cpp
+++ src/Flip3DComp_View.cpp
@@ -100,8 +100,8 @@ void Flip3DComp::SelectWindow(HWND hwndTarget)
 
     if (m_cards[(size_t)selIdx].m_isMinimized)
     {
-        ShowWindowAsync(hwndTarget, SW_SHOWNOACTIVATE);
-        SendMessage(hwndTarget, WM_SYSCOMMAND, SC_RESTORE, 0);
+        //ShowWindowAsync(hwndTarget, SW_SHOWNOACTIVATE);
+        //SendMessage(hwndTarget, WM_SYSCOMMAND, SC_RESTORE, 0);
         //UpdateCardGeometry(m_cards[(size_t)selIdx], m_monW, m_monH, /*selectedRestore=*/true);
         UpdateRestoredMinimizedCardGeometry(m_cards[(size_t)selIdx], m_monW, m_monH);
     }
