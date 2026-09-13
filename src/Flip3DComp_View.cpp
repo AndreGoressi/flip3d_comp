1e21b4f42899a1932e596f2001d15d81797754c8
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-13 23:42:05 +0200

    Modify window visibility handling for minimized cards

--- src/Flip3DComp_View.cpp
+++ src/Flip3DComp_View.cpp
@@ -100,7 +100,10 @@ void Flip3DComp::SelectWindow(HWND hwndTarget)
 
     if (m_cards[(size_t)selIdx].m_isMinimized)
     {
+        ShowWindow(hwndTarget, SW_HIDE);
+        SendMessage(hwndTarget, WM_SYSCOMMAND, SC_RESTORE, 0);
         UpdateRestoredMinimizedCardGeometry(m_cards[(size_t)selIdx], m_monW, m_monH);
+        ShowWindow(hwndTarget, SW_SHOWNA);
     }
 
     m_selectedHwnd = hwndTarget;
