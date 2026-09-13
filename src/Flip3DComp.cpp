2592cd25e67c440f5b5638f9991b8b55670678b6
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-13 23:29:45 +0200

    Handle WM_NCHITTEST for transparent area detection

--- src/Flip3DComp.cpp
+++ src/Flip3DComp.cpp
@@ -158,6 +158,21 @@ LRESULT Flip3DComp::HandleMessage(UINT msg, WPARAM wParam, LPARAM lParam)
         OnWheel(GET_WHEEL_DELTA_WPARAM(wParam));
         return 0;
 
+    case WM_NCHITTEST:
+    {
+        POINT pt = { (LONG)(short)LOWORD(lParam), (LONG)(short)HIWORD(lParam) };
+        HMONITOR hMon = MonitorFromPoint(pt, MONITOR_DEFAULTTONEAREST);
+        MONITORINFO mi = { sizeof(mi) };
+        if (hMon && GetMonitorInfoW(hMon, &mi))
+        {
+            if (pt.y >= mi.rcMonitor.bottom - 2)
+            {
+                return HTTRANSPARENT;
+            }
+        }
+        break;
+    }
+
     case WM_MOUSEMOVE:
         m_hitHwnd = HitTest3DScene(
             (LONG)(short)LOWORD(lParam),
