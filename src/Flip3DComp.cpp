b1eba56662406af4d5b8e097fe850a0e53373dbf
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-13 21:51:45 +0200

    Implement WM_NCHITTEST for bottom edge transparency
    Add hit testing for the bottom edge of the window.

--- src/Flip3DComp.cpp
+++ src/Flip3DComp.cpp
@@ -154,6 +154,15 @@ LRESULT Flip3DComp::HandleMessage(UINT msg, WPARAM wParam, LPARAM lParam)
         m_thumbnailsDirty = true;
         return 0;
 
+    case WM_NCHITTEST:
+        POINT pt = { (LONG)(short)LOWORD(lParam), (LONG)(short)HIWORD(lParam) };
+        HMONITOR hMon = MonitorFromPoint(pt, MONITOR_DEFAULTTONEAREST);
+        MONITORINFO mi = { sizeof(mi) };
+        if (hMon && GetMonitorInfoW(hMon, &mi))
+            if (pt.y >= mi.rcMonitor.bottom - 2)
+                return HTTRANSPARENT;
+        break;
+
     case WM_MOUSEWHEEL:
         OnWheel(GET_WHEEL_DELTA_WPARAM(wParam));
         return 0;
