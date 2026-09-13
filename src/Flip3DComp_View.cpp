349d955203aa9eda745b7c818700ec8dcda7c135
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-13 13:30:32 +0200

    Update window restoration for minimized cards
    Restore minimized card geometry when showing window.

--- src/Flip3DComp_View.cpp
+++ src/Flip3DComp_View.cpp
@@ -99,7 +99,12 @@ void Flip3DComp::SelectWindow(HWND hwndTarget)
     }
 
     if (m_cards[(size_t)selIdx].m_isMinimized)
-        UpdateCardGeometry(m_cards[(size_t)selIdx], m_monW, m_monH, /*selectedRestore=*/true);
+    {
+        ShowWindowAsync(hwndTarget, SW_SHOWNOACTIVATE);
+        SendMessage(hwndTarget, WM_SYSCOMMAND, SC_RESTORE, 0);
+        //UpdateCardGeometry(m_cards[(size_t)selIdx], m_monW, m_monH, /*selectedRestore=*/true);
+        UpdateRestoredMinimizedCardGeometry(m_cards[(size_t)selIdx], m_monW, m_monH);
+    }
 
     m_selectedHwnd = hwndTarget;
     m_lastPaintOrder.clear();
