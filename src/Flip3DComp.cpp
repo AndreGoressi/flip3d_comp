69f47c7bcd2398e7593da73a5a0c2fdd8714694f
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-13 21:29:04 +0200

    Change LivePreview activation to use Desktop type

--- src/Flip3DComp.cpp
+++ src/Flip3DComp.cpp
@@ -55,7 +55,7 @@ bool Flip3DComp::Initialize(HINSTANCE hInstance)
     m_animEnter.Restart(0.0f, 1.0f, kEnterExitDurationSec);
     m_prevFrame = std::chrono::steady_clock::now();
     //
-    LivePreview::Activate(TRUE, m_selectedHwnd, m_hwnd, PeekType::Window);
+    LivePreview::Activate(TRUE, m_selectedHwnd, m_hwnd, PeekType::Desktop);
 
     EnterFlip3DWindowMode();
     InitAccessibility();
