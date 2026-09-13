a8bb9e452b0d6d7d33ee007a91f1c0ac1499026e
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-13 23:13:53 +0200

    Change window show method to SW_SHOWNA

--- src/Flip3DComp_View.cpp
+++ src/Flip3DComp_View.cpp
@@ -115,9 +115,13 @@ void Flip3DComp::SelectWindow(HWND hwndTarget)
         {
             attached = (AttachThreadInput(currentThreadId, targetThreadId, TRUE) == TRUE);
         }
+
+        ShowWindow(hwndTarget, SW_HIDE);
+        SendMessageW(hwndTarget, WM_SYSCOMMAND, SC_RESTORE, 0);
         
         UpdateRestoredMinimizedCardGeometry(card, m_monW, m_monH);
-        ShowWindowAsync(hwndTarget, SW_SHOWNOACTIVATE);
+
+        ShowWindow(hwndTarget, SW_SHOWNA);
     
         if (attached)
         {
