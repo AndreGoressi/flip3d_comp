ee367461e6a89c5acdf7bf1204c16356eee572cc
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-13 21:22:02 +0200

    Update Flip3DComp_View.cpp

--- src/Flip3DComp_View.cpp
+++ src/Flip3DComp_View.cpp
@@ -15,7 +15,7 @@ void Flip3DComp::ExitView(bool commitScroll, float exitDurationSec)
     if (m_state == ViewState::Exit || m_state == ViewState::ExitRepeatedRotate)
         return;
 
-    LivePreview::Activate(FALSE, m_selectedHwnd, m_hwnd, PeekType::Desktop);
+    LivePreview::Activate(FALSE, m_selectedHwnd, m_hwnd, PeekType::Window);
 
     if (commitScroll)
         CommitCarouselScroll();
