14b36ae930c133c80256d71b983618876818846e
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-13 10:44:42 +0200

    Update Flip3DComp.cpp

--- src/Flip3DComp.cpp
+++ src/Flip3DComp.cpp
@@ -55,12 +55,6 @@ bool Flip3DComp::Initialize(HINSTANCE hInstance)
     m_animEnter.Restart(0.0f, 1.0f, kEnterExitDurationSec);
     m_prevFrame = std::chrono::steady_clock::now();
     //
-    HWND hwndProgman = FindWindowW(L"Progman", nullptr);
-    if (hwndProgman)
-    {
-        SetForegroundWindow(hwndProgman);
-    }
-    //
     LivePreview::Activate(TRUE, m_selectedHwnd, m_hwnd, PeekType::Desktop);
 
     EnterFlip3DWindowMode();
