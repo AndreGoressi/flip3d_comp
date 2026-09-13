d735caba84865f9715dadf5e71e6e931e18b8eef
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-13 23:48:34 +0200

    Update Flip3DComp_View.cpp

--- src/Flip3DComp_View.cpp
+++ src/Flip3DComp_View.cpp
@@ -77,12 +77,12 @@ void Flip3DComp::SelectWindow(HWND hwndTarget)
 
     const bool isShell = (hwndTarget == GetShellWindow());
 
-    if (isShell)
+    /*if (isShell)
     {
         if (HWND shellTray = FindWindowW(L"Shell_TrayWnd", nullptr))
             PostMessageW(shellTray, 0x579, 1, 0);
     }
-    else if (!IsWindowEnabled(hwndTarget))
+    else*/ if (!IsWindowEnabled(hwndTarget))
     {
         SwitchToThisWindow(GetLastActivePopup(GetAncestor(hwndTarget, GA_ROOTOWNER)), TRUE);
     }
