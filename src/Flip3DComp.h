0c3fb183651de5180d3fb8bc9c4458e2817ba575
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-14 07:30:10 +0200

    Update Flip3DComp.h

--- src/Flip3DComp.h
+++ src/Flip3DComp.h
@@ -127,7 +127,7 @@ private:
     void    OnThumbnailSourceSizeChanged();
     void    UpdateCardThumbnailDest(CardModel& card);
     HRESULT CreateCardVisuals();
-    void CheckPendingThumbnails();
+    bool CheckPendingThumbnail(HWND hwnd);
     // ========================================================================
     // Per-frame update
     // ========================================================================
