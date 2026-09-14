7c6ed2a6625c2d9324c0643705af2fb7d535904f
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-14 04:19:35 +0200

    Add UpdateMultiWindowVisualExclusion call in Update

--- src/Flip3DComp_Update.cpp
+++ src/Flip3DComp_Update.cpp
@@ -572,6 +572,7 @@ void Flip3DComp::Update(float dtSeconds)
 {
     if (m_thumbnailsDirty)
         OnThumbnailSourceSizeChanged();
+        UpdateMultiWindowVisualExclusion();
 
     if (GetAsyncKeyState(VK_SHIFT) & 0x8000)
         dtSeconds *= 0.05f;
