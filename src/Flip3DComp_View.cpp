b9409a825e8a6bbbe3e128dd13648388cf20420b
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-15 01:01:17 +0200

    Modify DWM thumbnail properties in Flip3DComp_View
    Updated thumbnail properties to include destination rectangle.

--- src/Flip3DComp_View.cpp
+++ src/Flip3DComp_View.cpp
@@ -142,11 +142,12 @@ void Flip3DComp::SelectWindow(HWND hwndTarget)
         {
             DwmRegisterThumbnail(m_hwnd, hwndTarget, &card.m_hThumb);
             //
-            DWM_THUMBNAIL_PROPERTIES props = {};
-            props.dwFlags = DWM_TNP_VISIBLE | DWM_TNP_OPACITY;
-            props.fVisible = TRUE;
-            props.opacity = 255;
-            DwmUpdateThumbnailProperties(card.m_hThumb, &props);
+            DWM_THUMBNAIL_PROPERTIES thp = {};
+            thp.dwFlags = DWM_TNP_VISIBLE | DWM_TNP_OPACITY | DWM_TNP_RECTDESTINATION;
+            thp.fVisible = TRUE;
+            thp.opacity = 255;
+            thp.rcDestination = { 0, 0, card.m_srcWidth, card.m_srcHeight };
+            DwmUpdateThumbnailProperties(card.m_hThumb, &thp);
             DwmFlush();
         }
     }
