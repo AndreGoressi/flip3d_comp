62c3b05b4c415ca9b64e7f7957abfc4011899188
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-14 18:47:34 +0200

    Update Flip3DComp_View.cpp

--- src/Flip3DComp_View.cpp
+++ src/Flip3DComp_View.cpp
@@ -100,7 +100,6 @@ void Flip3DComp::SelectWindow(HWND hwndTarget)
 
     if (m_cards[(size_t)selIdx].m_isMinimized)
     {
-        UpdateCardGeometry(m_cards[(size_t)selIdx], m_monW, m_monH, /*selectedRestore=*/true);
         CheckPendingThumbnail(hwndTarget);
 
         DWORD targetThreadId = GetWindowThreadProcessId(hwndTarget, nullptr);
@@ -112,7 +111,7 @@ void Flip3DComp::SelectWindow(HWND hwndTarget)
             attached = AttachThreadInput(currentThreadId, targetThreadId, TRUE);
         }
 
-        ShowWindowAsync(hwndTarget, SW_RESTORE);
+        UpdateCardGeometry(m_cards[(size_t)selIdx], m_monW, m_monH, /*selectedRestore=*/true);
 
         if (attached)
         {
