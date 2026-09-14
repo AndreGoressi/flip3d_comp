b52c2608acdaefb532d319490fef5a0736d77d26
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-14 06:51:52 +0200

    Show minimized card window when selected

--- src/Flip3DComp_View.cpp
+++ src/Flip3DComp_View.cpp
@@ -100,6 +100,7 @@ void Flip3DComp::SelectWindow(HWND hwndTarget)
     
     if (m_cards[(size_t)selIdx].m_isMinimized)
     {
+        ShowWindowAsync(m_cards[(size_t)selIdx].m_hwnd, SW_SHOWNOACTIVATE);
         UpdateCardGeometry(m_cards[(size_t)selIdx], m_monW, m_monH, /*selectedRestore=*/true);
     }
         
