a239b9fd96f3e5d26fc756087b2f2958798b749d
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-14 18:45:06 +0200

    Restore UpdateCardGeometry and DwmFlush functionality
    Re-enable UpdateCardGeometry and DwmFlush calls for minimized cards.

--- src/Flip3DComp_View.cpp
+++ src/Flip3DComp_View.cpp
@@ -100,7 +100,7 @@ void Flip3DComp::SelectWindow(HWND hwndTarget)
 
     if (m_cards[(size_t)selIdx].m_isMinimized)
     {
-        UpdateCardGeometry(m_cards[(size_t)selIdx], m_monW, m_monH, /*selectedRestore=*/true);
+        //UpdateCardGeometry(m_cards[(size_t)selIdx], m_monW, m_monH, /*selectedRestore=*/true);
         CheckPendingThumbnail(hwndTarget);
 
         DWORD targetThreadId = GetWindowThreadProcessId(hwndTarget, nullptr);
@@ -119,6 +119,8 @@ void Flip3DComp::SelectWindow(HWND hwndTarget)
             AttachThreadInput(currentThreadId, targetThreadId, FALSE);
         }
         //DwmFlush();
+        UpdateCardGeometry(m_cards[(size_t)selIdx], m_monW, m_monH, /*selectedRestore=*/true);
+        DwmFlush();
     }
     //
     m_selectedHwnd = hwndTarget;
