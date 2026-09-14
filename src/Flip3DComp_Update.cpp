d275c2223f41ef10fc6dab5ffe9ae27ac37fb2e1
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-14 04:25:58 +0200

    Remove UpdateMultiWindowVisualExclusion call
    Removed call to UpdateMultiWindowVisualExclusion in Update method.

--- src/Flip3DComp_Update.cpp
+++ src/Flip3DComp_Update.cpp
@@ -572,7 +572,6 @@ void Flip3DComp::Update(float dtSeconds)
 {
     if (m_thumbnailsDirty)
         OnThumbnailSourceSizeChanged();
-        UpdateMultiWindowVisualExclusion();
 
     if (GetAsyncKeyState(VK_SHIFT) & 0x8000)
         dtSeconds *= 0.05f;
