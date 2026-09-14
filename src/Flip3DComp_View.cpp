7e62a5c01ca91284342a35b553b1ab85d681cc08
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-14 06:15:17 +0200

    Refactor Flip3DComp_View by removing redundant calls
    Removed unnecessary DwmInvalidateIconicBitmaps call and Commit from dcompDevice.

--- src/Flip3DComp_View.cpp
+++ src/Flip3DComp_View.cpp
@@ -100,11 +100,7 @@ void Flip3DComp::SelectWindow(HWND hwndTarget)
     
     if (m_cards[(size_t)selIdx].m_isMinimized)
     {
-        DwmInvalidateIconicBitmaps(m_cards[(size_t)selIdx].m_hwnd);
         UpdateCardGeometry(m_cards[(size_t)selIdx], m_monW, m_monH, /*selectedRestore=*/true);
-    
-        if (m_dcompDevice)
-            m_dcompDevice->Commit();
     }
         
     m_selectedHwnd = hwndTarget;
