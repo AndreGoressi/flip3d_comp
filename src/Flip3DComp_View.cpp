5a57c0f38804b8584a7da592bf7775141bdedd09
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-15 01:10:06 +0200

    Update Flip3DComp_View.cpp

--- src/Flip3DComp_View.cpp
+++ src/Flip3DComp_View.cpp
@@ -143,9 +143,8 @@ void Flip3DComp::SelectWindow(HWND hwndTarget)
             DwmRegisterThumbnail(m_hwnd, hwndTarget, &card.m_hThumb);
             //
             DWM_THUMBNAIL_PROPERTIES thp = {};
-            thp.dwFlags = DWM_TNP_VISIBLE | DWM_TNP_OPACITY | DWM_TNP_RECTDESTINATION;
+            thp.dwFlags = DWM_TNP_VISIBLE | DWM_TNP_RECTDESTINATION;
             thp.fVisible = TRUE;
-            thp.opacity = 0;
             thp.rcDestination = { 0, 0, card.m_srcWidth, card.m_srcHeight };
             DwmUpdateThumbnailProperties(card.m_hThumb, &thp);
             DwmFlush();
