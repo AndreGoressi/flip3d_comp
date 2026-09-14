26540e7b639d86a4792ec3686b36b7912c0d901e
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-14 17:55:38 +0200

    Disable cursor position handling in Flip3DComp_View
    Comment out mouse position handling code.

--- src/Flip3DComp_View.cpp
+++ src/Flip3DComp_View.cpp
@@ -91,11 +91,11 @@ void Flip3DComp::ThumbnailAsWindowToForeground(HWND hWnd)
         AttachThreadInput(dwCurrentThreadId, dwTargetThreadId, FALSE);
     }
 
-    POINT mousePos;
+    /*POINT mousePos;
     if (GetCursorPos(&mousePos))
     {
         SetCursorPos(mousePos.x, mousePos.y);
-    }
+    }*/
     DwmFlush();
 }
 
