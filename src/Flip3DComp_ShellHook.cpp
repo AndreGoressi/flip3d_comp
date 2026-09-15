a55cdfc135cd5faabb556b551d17daa9a939e1cf
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-15 18:15:32 +0200

    Update Flip3DComp_ShellHook.cpp

--- src/Flip3DComp_ShellHook.cpp
+++ src/Flip3DComp_ShellHook.cpp
@@ -224,11 +224,11 @@ HRESULT Flip3DComp::CreateCardVisual(CardModel& card)
         container->SetClip(clip.Get());
     }
 
-    container->SetBorderMode(DCOMPOSITION_BORDER_MODE_SOFT);
-    container->SetBitmapInterpolationMode(DCOMPOSITION_BITMAP_INTERPOLATION_MODE_LINEAR);
+    container->SetBorderMode(DCOMPOSITION_BORDER_MODE_INHERIT);
+    container->SetBitmapInterpolationMode(DCOMPOSITION_BITMAP_INTERPOLATION_MODE_INHERIT);
 
-    card.m_visual->SetBorderMode(DCOMPOSITION_BORDER_MODE_SOFT);
-    card.m_visual->SetBitmapInterpolationMode(DCOMPOSITION_BITMAP_INTERPOLATION_MODE_LINEAR);
+    card.m_visual->SetBorderMode(DCOMPOSITION_BORDER_MODE_INHERIT);
+    card.m_visual->SetBitmapInterpolationMode(DCOMPOSITION_BITMAP_INTERPOLATION_MODE_INHERIT);
 
     hr = container->AddVisual(card.m_visual.Get(), FALSE, nullptr);
     if (FAILED(hr))
