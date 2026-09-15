37c8db56a54dcc044b26cba8e559b5e55bd44370
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-16 01:22:57 +0200

    Update Flip3DComp_Composition.cpp

--- src/Flip3DComp_Composition.cpp
+++ src/Flip3DComp_Composition.cpp
@@ -43,7 +43,7 @@ HRESULT Flip3DComp::InitComposition()
         return hr;
     sceneBase.As(&m_sceneVisual);
     //
-    m_sceneVisual->SetDepthMode(DCOMPOSITION_DEPTH_MODE_INHERIT); //DCOMPOSITION_DEPTH_MODE_SORTED test: DCOMPOSITION_DEPTH_MODE_INHERIT 
+    m_sceneVisual->SetDepthMode(DCOMPOSITION_DEPTH_MODE_SORTED);
     //
     ComPtr<IDCompositionVisual> rootBase;
     root.As(&rootBase);
