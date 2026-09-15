460fa9ae8031b2609d23e7c726e77ba23cc3a99a
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-15 19:15:06 +0200

    Update Config.h

--- src/Config.h
+++ src/Config.h
@@ -95,9 +95,9 @@ constexpr float CardThumbnailQualityScale()
     case CardThumbnailQuality::Low:    return 0.25f;
     case CardThumbnailQuality::Medium: return 0.50f;
     case CardThumbnailQuality::High:    return 0.75f;
-    case CardThumbnailQuality::Native:   return 2.00f;
+    case CardThumbnailQuality::Native:   return 1.00f;
     }
-    //return; //test
+    return 1.00f;
 }
 
 // Normalization Bezier coefficients (quadratic)
