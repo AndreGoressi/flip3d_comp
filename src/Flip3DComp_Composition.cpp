d9bde7643b0f5689f95d47ff78641d75b2f088b1
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-16 01:29:04 +0200

    Change depth mode from SORTED to TREE

--- src/Flip3DComp_Composition.cpp
+++ src/Flip3DComp_Composition.cpp
@@ -43,7 +43,7 @@ HRESULT Flip3DComp::InitComposition()
         return hr;
     sceneBase.As(&m_sceneVisual);
     //
-    m_sceneVisual->SetDepthMode(DCOMPOSITION_DEPTH_MODE_SORTED);
+    m_sceneVisual->SetDepthMode(DCOMPOSITION_DEPTH_MODE_TREE);
     //
     ComPtr<IDCompositionVisual> rootBase;
     root.As(&rootBase);
