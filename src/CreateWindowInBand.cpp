71f49b046dad00103c968e408b30becbb4da7621
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-16 00:34:16 +0200

    Add files via upload

---
+++ src/CreateWindowInBand.cpp
@@ -0,0 +1,52 @@
+#include "CreateWindowInBand.h"
+
+static CreateWindowInBand_t g_CreateWindowInBand = nullptr;
+
+bool WindowInBand::Initialize()
+{
+    if (g_CreateWindowInBand)
+        return true;
+
+    auto user32 = GetModuleHandleW(L"user32.dll");
+
+    if (!user32)
+        return false;
+
+    g_CreateWindowInBand =
+        reinterpret_cast<CreateWindowInBand_t>(
+            GetProcAddress(
+                user32,
+                "CreateWindowInBand"));
+
+    return g_CreateWindowInBand != nullptr;
+}
+
+HWND WindowInBand::CreateWindowInBand(DWORD exStyle,
+                                    ATOM atom,
+                                    LPCWSTR title,
+                                    DWORD style,
+                                    int x,
+                                    int y,
+                                    int width,
+                                    int height,
+                                    HINSTANCE instance,
+                                    LPVOID param,
+                                    DWORD band)
+{
+    if (!Initialize())
+        return nullptr;
+
+    return g_CreateWindowInBand(exStyle,
+                                atom,
+                                title,
+                                style,
+                                x,
+                                y,
+                                width,
+                                height,
+                                nullptr,
+                                nullptr,
+                                instance,
+                                param,
+                                band);
+}
