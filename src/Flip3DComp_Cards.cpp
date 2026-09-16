daf2c3b7ed402bad06cb40b7180b3edbca34e819
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-16 16:53:05 +0200

    Swap card geometry update functions

--- src/Flip3DComp_Cards.cpp
+++ src/Flip3DComp_Cards.cpp
@@ -491,8 +491,8 @@ void Flip3DComp::BuildCards()
         CardModel c;
         c.m_hwnd                 = h;
         c.m_initialCarouselIndex = carouselIndex++;
-        UpdateCardGeometry(c, m_monW, m_monH);
-        //UpdateNormalCardGeometry(c, m_monW, m_monH);
+        //UpdateCardGeometry(c, m_monW, m_monH);
+        UpdateNormalCardGeometry(c, m_monW, m_monH);
         m_cards.push_back(std::move(c));
     }
 
