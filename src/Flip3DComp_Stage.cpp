10d4f504b0a3ea1d0708b39509ea4299e2db0cbc
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-16 05:01:43 +0200

    Update window positioning to use virtual screen metrics
    Replaced monitor dimensions retrieval with system metrics for virtual screen.

--- src/Flip3DComp_Stage.cpp
+++ src/Flip3DComp_Stage.cpp
@@ -66,10 +66,14 @@ void Flip3DComp::ApplyFullscreenLayout()
     if (hMon)
         GetMonitorInfoW(hMon, &mi);
     //
-    const int x = mi.rcMonitor.left;
+    /*const int x = mi.rcMonitor.left;
     const int y = mi.rcMonitor.top;
     const int w = mi.rcMonitor.right - mi.rcMonitor.left;
-    const int h = mi.rcMonitor.bottom - mi.rcMonitor.top;
+    const int h = mi.rcMonitor.bottom - mi.rcMonitor.top;*/
+    const int x = GetSystemMetrics(SM_XVIRTUALSCREEN);
+    const int y = GetSystemMetrics(SM_YVIRTUALSCREEN);
+    const int w = GetSystemMetrics(SM_CXVIRTUALSCREEN);
+    const int h = GetSystemMetrics(SM_CYVIRTUALSCREEN);
     //
     SetWindowPos(m_hwnd, HWND_TOP, x, y, w, h, SWP_SHOWWINDOW);
     //
@@ -142,14 +146,10 @@ bool Flip3DComp::InitializeDCompStage()
     if (hMon)
         GetMonitorInfoW(hMon, &mi);
 
-    /*const int x = mi.rcWork.left;
+    const int x = mi.rcWork.left;
     const int y = mi.rcWork.top;
     const int w = mi.rcWork.right - mi.rcWork.left;
-    const int h = mi.rcWork.bottom - mi.rcWork.top;*/
-    int x = GetSystemMetrics(SM_XVIRTUALSCREEN);
-    int y = GetSystemMetrics(SM_YVIRTUALSCREEN);
-    int w = GetSystemMetrics(SM_CXVIRTUALSCREEN);
-    int h = GetSystemMetrics(SM_CYVIRTUALSCREEN);
+    const int h = mi.rcWork.bottom - mi.rcWork.top;
     SetWindowPos(m_hwnd, HWND_TOPMOST, x, y, w, h, SWP_SHOWWINDOW | SWP_NOACTIVATE);
     //
     HWND taskbar = FindWindowW(L"Shell_TrayWnd", nullptr);
