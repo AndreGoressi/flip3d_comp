74431212e151fa3e691c5d826f81f8c86a0597f9
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-16 04:40:13 +0200

    Change ZBID_UIACCESS to ZBID_DESKTOP

--- src/Flip3DComp_Stage.cpp
+++ src/Flip3DComp_Stage.cpp
@@ -141,7 +141,7 @@ bool Flip3DComp::InitializeDCompStage()
                                          0, 0, 0, 0,
                                          m_hInstance,
                                          this,
-                                         ZBID_UIACCESS
+                                         ZBID_DESKTOP
     );
     //
     if (!m_hwnd)
