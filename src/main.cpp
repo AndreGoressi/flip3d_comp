a403a99a4789c7206547adff2f05fc4dbf7b117f
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-16 05:53:15 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,41 @@
 // ============================================================================
 #include "Flip3DComp.h"
 
+/*int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nShowCmd)
+{
+    Flip3DComp main;
+
+    if (!main.Initialize(hInstance))
+    {
+        MessageBoxW(nullptr,
+            main.InitErrorMessage(),
+            L"Flip3D (DComp)", MB_OK | MB_ICONERROR);
+        return 1;
+    }
+
+    ShowWindow(main.WindowHandle(), SW_SHOW);
+    SetForegroundWindow(main.WindowHandle());
+    UpdateWindow(main.WindowHandle());
+
+    return main.Run();
+}*/
+
+// ============================================================================
+// main.cpp — Flip3D (DComp) entry point
+// ============================================================================
+#include "Flip3DComp.h"
+#include "PrepareForUIAccess.h" 
+
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nShowCmd)
 {
+    //Force UIAccess to run immediately at startup (restarts the EXE invisibly if necessary)[cite: 3]
+    DWORD dwErr = PrepareForUIAccess();
+    if (ERROR_SUCCESS != dwErr)
+    {
+        #if defined(_DEBUG)
+        #endif
+    }
+
     Flip3DComp main;
 
     if (!main.Initialize(hInstance))
