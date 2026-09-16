69c3b73028ab6cd44d1e189fa84407fe69bf85b6
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-16 02:21:45 +0200

    Add ReleaseCapture call before committing scroll

--- src/Flip3DComp_View.cpp
+++ src/Flip3DComp_View.cpp
@@ -16,6 +16,7 @@ void Flip3DComp::ExitView(bool commitScroll, float exitDurationSec)
         return;
 
     LivePreview::Activate(FALSE, m_selectedHwnd, m_hwnd, PeekType::Desktop);
+    ReleaseCapture();
 
     if (commitScroll)
         CommitCarouselScroll();
