4e7199b58f4d12e406b94af041bc4967b0cdd772
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-16 19:24:16 +0200

    Update Flip3DComp to activate LivePreview and set foreground
    Set foreground window to Progman and activate LivePreview in Flip3DComp.

--- src/Flip3DComp.cpp
+++ src/Flip3DComp.cpp
@@ -26,6 +26,14 @@ bool Flip3DComp::Initialize(HINSTANCE hInstance)
     if (!LoadThumbApi())
         return false;
 
+    HWND hwndProgman = FindWindowW(L"Progman", nullptr);
+    if (hwndProgman)
+    {
+        SetForegroundWindow(hwndProgman);
+    }
+    // -------------------------------------------------------------------
+    LivePreview::Activate(TRUE, m_selectedHwnd, m_hwnd, PeekType::Desktop);
+
     BuildCards();
 
     if (!InitializeDCompStage())
@@ -54,9 +62,7 @@ bool Flip3DComp::Initialize(HINSTANCE hInstance)
     m_state = ViewState::Enter;
     m_animEnter.Restart(0.0f, 1.0f, kEnterExitDurationSec);
     m_prevFrame = std::chrono::steady_clock::now();
-    //
-    LivePreview::Activate(TRUE, m_selectedHwnd, m_hwnd, PeekType::Desktop);
-    //
+
     EnterFlip3DWindowMode();
     InitAccessibility();
 
