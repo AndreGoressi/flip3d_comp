10b596d5960cd127460527c9e7646423baf7b068
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-17 08:34:16 +0200

    Update Flip3DComp_Composition.cpp

--- src/Flip3DComp_Composition.cpp
+++ src/Flip3DComp_Composition.cpp
@@ -65,7 +65,7 @@ HRESULT CreateSharedWashSurface(ID3D11Device* d3d,
         {
             ComPtr<ID3D11DeviceContext> ctx;
             d3d->GetImmediateContext(&ctx);
-            const float wash[4] = { 0.07f, 0.08f, 0.11f, 0.85f }; 
+            const float wash[4] = { 0.04f, 0.05f, 0.08f, 0.5f };
             ctx->ClearRenderTargetView(rtv.Get(), wash);
         }
         bg->EndDraw();
