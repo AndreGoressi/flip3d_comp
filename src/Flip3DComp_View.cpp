0e9c911ce6a483b199aee4498663fd4baf63f54b
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-17 06:33:04 +0200

    Fix LivePreview activation with target window

--- src/Flip3DComp_View.cpp
+++ src/Flip3DComp_View.cpp
@@ -144,7 +144,7 @@ void Flip3DComp::SelectWindow(HWND hwndTarget)
     if (!hwndTarget || !IsWindow(hwndTarget))
         return;
 
-    LivePreview::Activate(FALSE, m_selectedHwnd, m_hwnd, PeekType::Window);
+    LivePreview::Activate(FALSE, hwndTarget, m_hwnd, PeekType::Window);
     //
     const bool isShell = (hwndTarget == GetShellWindow());
     if (isShell)
