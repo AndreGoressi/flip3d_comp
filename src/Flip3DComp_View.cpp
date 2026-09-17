cc9b242109dcf6cfb3a2c2c7279ce7a947dc15da
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-17 10:19:05 +0200

    Fix commented code structure in Flip3DComp_View.cpp

--- src/Flip3DComp_View.cpp
+++ src/Flip3DComp_View.cpp
@@ -187,8 +187,8 @@ void Flip3DComp::SelectWindow(HWND hwndTarget)
     {
         if (HWND shellTray = FindWindowW(L"Shell_TrayWnd", nullptr))
             PostMessageW(shellTray, 0x579, 1, 0);
-    }*/
-    else if (!IsWindowEnabled(hwndTarget))
+    }
+    else*/if (!IsWindowEnabled(hwndTarget))
     {
         SwitchToThisWindow(GetLastActivePopup(GetAncestor(hwndTarget, GA_ROOTOWNER)), TRUE);
     }
