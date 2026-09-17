48f1b7ff1db93e5faf606011714725c1b1737c76
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-17 19:43:29 +0200

    Refactor class comparison in Flip3DComp_ShellHook
    Removed redundant check for 'Progman' in class comparison.

--- src/Flip3DComp_ShellHook.cpp
+++ src/Flip3DComp_ShellHook.cpp
@@ -88,8 +88,7 @@ bool Flip3DComp::IsNeverHiddenWindow(HWND hwnd) const
 
     return !_wcsicmp(cls, L"Shell_TrayWnd")
         || !_wcsicmp(cls, L"Shell_SecondaryTrayWnd")
-        || !_wcsicmp(cls, L"WorkerW")
-        || !_wcsicmp(cls, L"Progman");
+        || !_wcsicmp(cls, L"WorkerW");
 }
 
 // ============================================================================
