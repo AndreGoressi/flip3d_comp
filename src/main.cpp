dcaf74a0bc2bb68702fd854855962344be414877
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-19 00:58:06 +0200

    Refactor main function and include necessary headers

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,24 @@
 // ============================================================================
 // main.cpp — Flip3D (DComp) entry point
 // ============================================================================
+#include <windows.h>
+#include "PrepareForUIAccess.h"
 #include "Flip3DComp.h"
-#include "PrepareForUIAccess.h" 
 
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nShowCmd)
+int main()
 {
-    //Force UIAccess to run immediately at startup (restarts the EXE invisibly if necessary)[cite: 3]
     DWORD dwErr = PrepareForUIAccess();
     if (ERROR_SUCCESS != dwErr)
     {
-        //#if defined(_DEBUG)
-        //#endif
+        //...
     }
 
+    HINSTANCE hInstance = GetModuleHandle(nullptr);
     Flip3DComp main;
 
     if (!main.Initialize(hInstance))
     {
-        MessageBoxW(nullptr,
-            main.InitErrorMessage(),
-            L"Flip3D (DComp)", MB_OK | MB_ICONERROR);
+        MessageBoxW(nullptr, main.InitErrorMessage(), L"Flip3D (DComp)", MB_OK | MB_ICONERROR);
         return 1;
     }
     ShowWindow(main.WindowHandle(), SW_SHOW);
