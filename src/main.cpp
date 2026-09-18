17f69fd89eb31fa03e40fe4ae919e04119d900ec
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-19 00:43:34 +0200

    Remove unnecessary windows.h include

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 // ============================================================================
 // main.cpp — Flip3D (DComp) entry point
 // ============================================================================
-#include <windows.h>
 #include "PrepareForUIAccess.h"
 #include "Flip3DComp.h"
 
