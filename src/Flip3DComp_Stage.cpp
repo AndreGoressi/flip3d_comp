2985e711b989b2d3f7ccdf79858e76936e22e425
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-19 00:03:41 +0200

    Update Flip3DComp_Stage.cpp

--- src/Flip3DComp_Stage.cpp
+++ src/Flip3DComp_Stage.cpp
@@ -70,7 +70,7 @@ void Flip3DComp::ApplyFullscreenLayout()
     const int y = mi.rcWork.top;
     const int w = mi.rcWork.right - mi.rcWork.left;
     const int h = mi.rcWork.bottom - mi.rcWork.top;
-    SetWindowPos(m_hwnd, HWND_TOPMOST, x, y, w, h, SWP_SHOWWINDOW | SWP_NOACTIVATE);
+    SetWindowPos(m_hwnd, HWND_TOPMOST, x, y, w, h, SWP_SHOWWINDOW);
 
     RECT client = {};
     if (GetClientRect(m_hwnd, &client))
