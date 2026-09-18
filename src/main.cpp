b57c7ba69880f448f345a00b1fac6c96c6be9839
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-19 00:41:21 +0200

    Refactor entry point and update main function

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,30 @@
 // ============================================================================
 // main.cpp — Flip3D (DComp) entry point
 // ============================================================================
+#include <windows.h>
+#include "uiaccess.h"
 #include "Flip3DComp.h"
-#include "PrepareForUIAccess.h" 
-/*int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nShowCmd)
+
+int APIENTRY _tWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPTSTR lpCmdLine, _In_ int nCmdShow)
 {
-    Flip3DComp main;
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+    UNREFERENCED_PARAMETER(nCmdShow);
 
-    if (!main.Initialize(hInstance))
-    {
-        MessageBoxW(nullptr,
-            main.InitErrorMessage(),
-            L"Flip3D (DComp)", MB_OK | MB_ICONERROR);
-        return 1;
-    }
-
-    ShowWindow(main.WindowHandle(), SW_SHOW);
-    SetForegroundWindow(main.WindowHandle());
-    UpdateWindow(main.WindowHandle());
-
-    return main.Run();
-}*/
-
-// ============================================================================
-// main.cpp — Flip3D (DComp) entry point
-// ============================================================================
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nShowCmd)
-{
     DWORD dwErr = PrepareForUIAccess();
     if (ERROR_SUCCESS != dwErr)
     {
         //...
     }
-
     Flip3DComp main;
 
     if (!main.Initialize(hInstance))
     {
-        MessageBoxW(nullptr,
-            main.InitErrorMessage(),
-            L"Flip3D (DComp)", MB_OK | MB_ICONERROR);
         return 1;
     }
     ShowWindow(main.WindowHandle(), SW_SHOW);
     SetForegroundWindow(main.WindowHandle());
     UpdateWindow(main.WindowHandle());
+
     return main.Run();
 }
