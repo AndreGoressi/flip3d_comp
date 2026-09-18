90b73cc40a554f6710a1d45fe53109c944c2b7d8
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-19 01:33:11 +0200

    Update monitor handling and window positioning
    Changed monitor retrieval method to use primary monitor and updated window positioning flags to prevent movement and resizing.

--- src/Flip3DComp_Stage.cpp
+++ src/Flip3DComp_Stage.cpp
@@ -62,7 +62,7 @@ void Flip3DComp::ApplyFullscreenLayout()
         return;
     
     MONITORINFO mi = { sizeof(mi) };
-    HMONITOR hMon = MonitorFromWindow(m_hwnd, MONITOR_DEFAULTTONEAREST);
+    HMONITOR hMon = MonitorFromWindow(m_hwnd, MONITOR_DEFAULTTOPRIMARY);
     if (hMon)
         GetMonitorInfoW(hMon, &mi);
 
@@ -70,7 +70,7 @@ void Flip3DComp::ApplyFullscreenLayout()
     const int y = mi.rcWork.top;
     const int w = mi.rcWork.right - mi.rcWork.left;
     const int h = mi.rcWork.bottom - mi.rcWork.top;
-    SetWindowPos(m_hwnd, HWND_TOPMOST, x, y, w, h, SWP_SHOWWINDOW);
+    SetWindowPos(m_hwnd, HWND_TOPMOST, x, y, w, h, SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE);
 
     RECT client = {};
     if (GetClientRect(m_hwnd, &client))
@@ -119,9 +119,7 @@ bool Flip3DComp::InitializeDCompStage()
         return false;
 
     MONITORINFO mi = { sizeof(mi) };
-    POINT pt;
-    GetCursorPos(&pt);
-    HMONITOR hMon = MonitorFromPoint(pt, MONITOR_DEFAULTTONEAREST);
+    HMONITOR hMon = MonitorFromWindow(m_hwnd, MONITOR_DEFAULTTOPRIMARY);
     if (hMon)
         GetMonitorInfoW(hMon, &mi);
 
@@ -130,7 +128,7 @@ bool Flip3DComp::InitializeDCompStage()
     const int w = mi.rcWork.right - mi.rcWork.left;
     const int h = mi.rcWork.bottom - mi.rcWork.top;
     //
-    SetWindowPos(m_hwnd, HWND_TOPMOST, x, y, w, h, SWP_SHOWWINDOW);
+    SetWindowPos(m_hwnd, HWND_TOPMOST, x, y, w, h, SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE);
     //
     BOOL exclude = TRUE;
     DwmSetWindowAttribute(m_hwnd, DWMWA_EXCLUDED_FROM_PEEK, &exclude, sizeof(exclude));
