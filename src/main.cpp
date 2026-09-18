6b573708b06da7acac1076abeb2812b18cfb8f4b
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-19 00:41:56 +0200

    Replace uiaccess.h with PrepareForUIAccess.h

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 // main.cpp — Flip3D (DComp) entry point
 // ============================================================================
 #include <windows.h>
-#include "uiaccess.h"
+#include "PrepareForUIAccess.h"
 #include "Flip3DComp.h"
 
 int APIENTRY _tWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPTSTR lpCmdLine, _In_ int nCmdShow)
