5e025aa49e55262f53ac6a5ccd42252cc1e7fdd3
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-18 20:06:22 +0200

    Uncomment RefreshDesktopGroupThumbnailsIfStale call

--- src/Flip3DComp_Update.cpp
+++ src/Flip3DComp_Update.cpp
@@ -617,7 +617,7 @@ void Flip3DComp::Update(float dtSeconds)
     if (m_thumbnailsDirty)
         OnThumbnailSourceSizeChanged();
 
-    //RefreshDesktopGroupThumbnailsIfStale();
+    RefreshDesktopGroupThumbnailsIfStale();
 
     if (GetAsyncKeyState(VK_SHIFT) & 0x8000)
         dtSeconds *= 0.05f;
