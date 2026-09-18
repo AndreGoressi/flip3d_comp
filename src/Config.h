bef478ab35d9159a47c2eef9a38dd1a272d77e89
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-18 15:40:02 +0200

    Update Config.h

--- src/Config.h
+++ src/Config.h
@@ -169,3 +169,6 @@ using DwmpCreateSharedMultiWindowVisual_fn = HRESULT (WINAPI *)(
 
 using DwmpUpdateSharedMultiWindowVisual_fn = HRESULT (WINAPI *)(
     HTHUMBNAIL hThumbnailId, HWND* phwndsInclude, DWORD chwndsInclude, HWND* phwndsExclude, DWORD chwndsExclude, RECT* prcSource, SIZE* pDestinationSize, DWORD dwFlags);
+
+using DwmActivateLivePreview_fn = HRESULT (WINAPI*)(
+    BOOL fEnable, HWND hwnd, RECT* prcQuery, DWORD dwUnused);
