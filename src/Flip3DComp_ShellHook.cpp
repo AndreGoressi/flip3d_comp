e8210c965722a067ec0f4933bbe7eccc830360c6
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-20 00:02:57 +0200

    Comment out topmost window style modifications
    Commented out code that modifies window styles for topmost behavior.

--- src/Flip3DComp_ShellHook.cpp
+++ src/Flip3DComp_ShellHook.cpp
@@ -70,9 +70,10 @@ BOOL CALLBACK Flip3DComp::RemoveTopmostCallback(HWND hwnd, LPARAM lParam)
     LONG_PTR exStyle = GetWindowLongPtrW(hwnd, GWL_EXSTYLE);
     if (exStyle & WS_EX_TOPMOST)
     {
-        SetWindowLongPtrW(hwnd, GWL_EXSTYLE, exStyle & ~WS_EX_TOPMOST);
+        /*SetWindowLongPtrW(hwnd, GWL_EXSTYLE, exStyle & ~WS_EX_TOPMOST);
         SetWindowPos(hwnd, HWND_NOTOPMOST, 0, 0, 0, 0, 
-                     SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE);
+                     SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE);*/
+        SetCapture(hwnd);
         
         s_strippedTopmostWindows.push_back(hwnd);
     }
@@ -85,11 +86,12 @@ void Flip3DComp::RestoreCompetingTopmost()
     {
         if (IsWindow(hwnd))
         {
-            LONG_PTR exStyle = GetWindowLongPtrW(hwnd, GWL_EXSTYLE);
+            ReleaseCapture();
+            /*LONG_PTR exStyle = GetWindowLongPtrW(hwnd, GWL_EXSTYLE);
             SetWindowLongPtrW(hwnd, GWL_EXSTYLE, exStyle | WS_EX_TOPMOST);
 
             SetWindowPos(hwnd, HWND_TOPMOST, 0, 0, 0, 0, 
-                         SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE);
+                         SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE);*/
         }
     }
     s_strippedTopmostWindows.clear();
