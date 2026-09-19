22c0777f62808e373ce9ca776888125d5c85f44c
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-19 23:38:07 +0200

    Refactor window style handling in Flip3DComp_ShellHook
    Removed commented-out SetLayeredWindowAttributes calls and adjusted SetWindowPos flags.

--- src/Flip3DComp_ShellHook.cpp
+++ src/Flip3DComp_ShellHook.cpp
@@ -71,10 +71,8 @@ BOOL CALLBACK Flip3DComp::RemoveTopmostCallback(HWND hwnd, LPARAM lParam)
     if (exStyle & WS_EX_TOPMOST)
     {
         SetWindowLongPtrW(hwnd, GWL_EXSTYLE, (exStyle & ~WS_EX_TOPMOST) | WS_EX_LAYERED | WS_EX_TRANSPARENT);
-        //SetLayeredWindowAttributes(hwnd, 0, 0, LWA_ALPHA);
-
         SetWindowPos(hwnd, HWND_NOTOPMOST, 0, 0, 0, 0, 
-                     SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE | SWP_FRAMECHANGED);
+                     SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE);
         
         s_strippedTopmostWindows.push_back(hwnd);
     }
@@ -89,9 +87,8 @@ void Flip3DComp::RestoreCompetingTopmost()
         {
             LONG_PTR exStyle = GetWindowLongPtrW(hwnd, GWL_EXSTYLE);
             SetWindowLongPtrW(hwnd, GWL_EXSTYLE, (exStyle | WS_EX_TOPMOST) & ~WS_EX_LAYERED & ~WS_EX_TRANSPARENT);
-            //SetLayeredWindowAttributes(hwnd, 0, 255, LWA_ALPHA);
             SetWindowPos(hwnd, HWND_TOPMOST, 0, 0, 0, 0, 
-                         SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE | SWP_FRAMECHANGED);
+                         SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE);
         }
     }
     s_strippedTopmostWindows.clear();
