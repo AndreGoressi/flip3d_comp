315a078a11bd04b72c594eb76a7214be12f40295
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-19 14:38:04 +0200

    Update Flip3DComp.h

--- src/Flip3DComp.h
+++ src/Flip3DComp.h
@@ -145,6 +145,7 @@ private:
     // ========================================================================
 
     float   EnterProgress() const;
+    void SetTopmostStatus(bool fAlwaysTop);
     void    ExitView(bool commitScroll = true,
                      float exitDurationSec = kExitDurationSec);
 
