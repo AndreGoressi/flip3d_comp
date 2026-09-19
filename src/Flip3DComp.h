a481456215137e7a3a3054ed6517350ef20cad99
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-19 14:25:58 +0200

    Update Flip3DComp.h

--- src/Flip3DComp.h
+++ src/Flip3DComp.h
@@ -59,6 +59,7 @@ public:
     bool    OnWheel(int wheelDelta);
     bool    OnMouse(LONG x, LONG y, bool pressed);
     bool    OnMouseMove(LONG x, LONG y);
+    bool IsHitTestValid() const { return m_hitHwnd != nullptr; }
     HWND WindowHandle() const { return m_hwnd; }
     const wchar_t* InitErrorMessage() const { return m_initError.c_str(); }
     int  Run();
