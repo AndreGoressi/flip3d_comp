5afad7e7cfa07a4dd4174eecb2b1c5b9ed6bbdb7
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-19 14:45:42 +0200

    Remove PrepareForUIAccess functionality
    Removed the inclusion of PrepareForUIAccess.h and its usage in wWinMain.

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,10 @@
 // main.cpp — Flip3D (DComp) entry point
 // ============================================================================
 #include "Flip3DComp.h"
-#include "PrepareForUIAccess.h"
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int)
 {
     CoInitializeEx(NULL, COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE);
-
-    DWORD dwErr = PrepareForUIAccess();
-
-    if (dwErr != ERROR_SUCCESS)
-    {
-        //...
-    }
 
     Flip3DComp main;
     if (!main.Initialize(hInstance))
