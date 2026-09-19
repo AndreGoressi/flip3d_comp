858a16bd09180e006ac4bba2e9fe4cb082f200b7
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-20 00:30:54 +0200

    Refactor topmost window handling in Flip3DComp_ShellHook
    Commented out code related to topmost window handling and added empty region setting for windows.

--- src/Flip3DComp_ShellHook.cpp
+++ src/Flip3DComp_ShellHook.cpp
@@ -67,10 +67,13 @@ BOOL CALLBACK Flip3DComp::RemoveTopmostCallback(HWND hwnd, LPARAM lParam)
     LONG_PTR exStyle = GetWindowLongPtrW(hwnd, GWL_EXSTYLE);
     if (exStyle & WS_EX_TOPMOST)
     {
-        SetWindowLongPtrW(hwnd, GWL_EXSTYLE, (exStyle & ~WS_EX_TOPMOST) | WS_EX_NOACTIVATE);
+        /*SetWindowLongPtrW(hwnd, GWL_EXSTYLE, exStyle & ~WS_EX_TOPMOST);
         SetWindowPos(hwnd, HWND_NOTOPMOST, 0, 0, 0, 0, 
-                     SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE);
+                     SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE);*/
         
+        HRGN hEmptyRgn = CreateRectRgn(0, 0, 0, 0);
+        SetWindowRgn(hwnd, hEmptyRgn, TRUE);
+
         s_strippedTopmostWindows.push_back(hwnd);
     }
     return TRUE;
@@ -82,10 +85,12 @@ void Flip3DComp::RestoreCompetingTopmost()
     {
         if (IsWindow(hwnd))
         {
-            LONG_PTR exStyle = GetWindowLongPtrW(hwnd, GWL_EXSTYLE);
-            SetWindowLongPtrW(hwnd, GWL_EXSTYLE, (exStyle | WS_EX_TOPMOST) & ~WS_EX_NOACTIVATE);
+            /*LONG_PTR exStyle = GetWindowLongPtrW(hwnd, GWL_EXSTYLE);
+            SetWindowLongPtrW(hwnd, GWL_EXSTYLE, exStyle | WS_EX_TOPMOST);
             SetWindowPos(hwnd, HWND_TOPMOST, 0, 0, 0, 0, 
-                         SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE);
+                         SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE);*/
+
+            SetWindowRgn(hwnd, nullptr, TRUE);
         }
     }
     s_strippedTopmostWindows.clear();
