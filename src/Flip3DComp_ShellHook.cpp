2ca3836beea61553be905cd498da5d9b7d69e233
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-20 00:09:53 +0200

    Clean up Flip3DComp_ShellHook.cpp code
    Removed commented-out code for setting window styles and capturing mouse events.

--- src/Flip3DComp_ShellHook.cpp
+++ src/Flip3DComp_ShellHook.cpp
@@ -70,11 +70,9 @@ BOOL CALLBACK Flip3DComp::RemoveTopmostCallback(HWND hwnd, LPARAM lParam)
     LONG_PTR exStyle = GetWindowLongPtrW(hwnd, GWL_EXSTYLE);
     if (exStyle & WS_EX_TOPMOST)
     {
-        /*SetWindowLongPtrW(hwnd, GWL_EXSTYLE, exStyle & ~WS_EX_TOPMOST);
+        SetWindowLongPtrW(hwnd, GWL_EXSTYLE, exStyle & ~WS_EX_TOPMOST);
         SetWindowPos(hwnd, HWND_NOTOPMOST, 0, 0, 0, 0, 
-                     SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE);*/
-        SetCapture(hwnd);
-        
+                     SWP_NOMOVE | SWP_NOSIZE | SWP_HIDEWINDOW | SWP_NOACTIVATE);
         s_strippedTopmostWindows.push_back(hwnd);
     }
     return TRUE;
@@ -86,12 +84,10 @@ void Flip3DComp::RestoreCompetingTopmost()
     {
         if (IsWindow(hwnd))
         {
-            ReleaseCapture();
-            /*LONG_PTR exStyle = GetWindowLongPtrW(hwnd, GWL_EXSTYLE);
+            LONG_PTR exStyle = GetWindowLongPtrW(hwnd, GWL_EXSTYLE);
             SetWindowLongPtrW(hwnd, GWL_EXSTYLE, exStyle | WS_EX_TOPMOST);
-
             SetWindowPos(hwnd, HWND_TOPMOST, 0, 0, 0, 0, 
-                         SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE);*/
+                         SWP_NOMOVE | SWP_NOSIZE | SWP_SHOWWINDOW | SWP_NOACTIVATE);
         }
     }
     s_strippedTopmostWindows.clear();
