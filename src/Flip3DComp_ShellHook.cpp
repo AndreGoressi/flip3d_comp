3549026ab08431bbbeb6bb79bb00535aa2bfb9ab
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-19 13:51:58 +0200

    Handle WM_MOUSEMOVE in Flip3DComp_ShellHook

--- src/Flip3DComp_ShellHook.cpp
+++ src/Flip3DComp_ShellHook.cpp
@@ -14,6 +14,7 @@ LRESULT CALLBACK LowLevelMouseProc(int nCode, WPARAM wParam, LPARAM lParam)
     if (nCode == HC_ACTION && s_instance)
     {
         const auto* info = reinterpret_cast<const MSLLHOOKSTRUCT*>(lParam);
+        
         if (wParam == WM_MOUSEWHEEL)
         {
             const int delta = GET_WHEEL_DELTA_WPARAM(info->mouseData);
@@ -25,6 +26,10 @@ LRESULT CALLBACK LowLevelMouseProc(int nCode, WPARAM wParam, LPARAM lParam)
             bool pressed = (wParam == WM_LBUTTONDOWN);
             s_instance->OnMouse(info->pt.x, info->pt.y, pressed);
             return 1; 
+        }
+        else if (wParam == WM_MOUSEMOVE)
+        {
+            return 1;
         }
     }
     return CallNextHookEx(nullptr, nCode, wParam, lParam);
