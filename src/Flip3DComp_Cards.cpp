2c5107a387f91bf7dba8a50eb9d19e4e6f379c72
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-20 20:37:13 +0200

    Clean up commented code in Flip3DComp_Cards.cpp
    Removed commented-out code related to window state checks and thumbnail properties.

--- src/Flip3DComp_Cards.cpp
+++ src/Flip3DComp_Cards.cpp
@@ -644,28 +644,9 @@ void Flip3DComp::RebuildDesktopGroupThumbnails(CardModel& card)
         for (HWND groupHwnd : group)
         {
             mix((uintptr_t)groupHwnd);
-            mix(IsIconic(groupHwnd) ? 1u : 0u);
+            //mix(IsIconic(groupHwnd) ? 1u : 0u);
 
             RECT rcWin = {};
-            //bool isMin = IsIconic(groupHwnd);*/
-
-            /*if (isMin)
-            {
-                WINDOWPLACEMENT wp = { sizeof(wp) };
-                if (GetWindowPlacement(groupHwnd, &wp))
-                {
-                    rcWin = wp.rcNormalPosition;
-                }
-            }
-            else
-            {
-                if (FAILED(DwmGetWindowAttribute(groupHwnd, DWMWA_EXTENDED_FRAME_BOUNDS, &rcWin, sizeof(rcWin))))
-                {
-                    GetWindowRect(groupHwnd, &rcWin);
-                }
-            }*/
-
-
             WINDOWPLACEMENT wp = { sizeof(wp) };
             if (GetWindowPlacement(groupHwnd, &wp))
             {
@@ -707,12 +688,6 @@ void Flip3DComp::RebuildDesktopGroupThumbnails(CardModel& card)
             HTHUMBNAIL subThumb = nullptr;
             DWM_THUMBNAIL_PROPERTIES subTp = {};
             subTp.dwFlags = DWM_TNP_VISIBLE | DWM_TNP_RECTDESTINATION | DWM_TNP_ENABLE3D | DWM_TNP_FORCECVI;
-
-            /*if (isMin)
-            {
-                subTp.dwFlags |= DWM_TNP_FORCECVI;
-            }*/
-
             subTp.fVisible = TRUE;
             subTp.rcDestination = { 0, 0, relW, relH };
 
