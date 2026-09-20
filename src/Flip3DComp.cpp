23b83cd0e9bc9c7d7d97297aa52e8ae6be7835b9
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-20 21:35:41 +0200

    Remove StripCompetingTopmost call
    Remove call to StripCompetingTopmost before entering view state.

--- src/Flip3DComp.cpp
+++ src/Flip3DComp.cpp
@@ -53,7 +53,6 @@ bool Flip3DComp::Initialize(HINSTANCE hInstance)
     {
         m_pfnActivateLivePreview(TRUE, m_hwnd, nullptr, static_cast<UINT>(PeekTypes::Desktop), nullptr);
     }
-    StripCompetingTopmost();
     
     m_state = ViewState::Enter;
     m_animEnter.Restart(0.0f, 1.0f, kEnterExitDurationSec);
