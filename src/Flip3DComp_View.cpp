b3b27fc45842204aedc14c146aa4bc5a749a776c
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-20 14:30:55 +0200

    Add LeaveInteractionOverride call in live preview

--- src/Flip3DComp_View.cpp
+++ src/Flip3DComp_View.cpp
@@ -17,6 +17,7 @@ void Flip3DComp::ExitView(bool commitScroll, float exitDurationSec)
     if (m_pfnActivateLivePreview)
     {
         m_pfnActivateLivePreview(FALSE, m_hwnd, nullptr, static_cast<UINT>(PeekTypes::Window), nullptr);
+        LeaveInteractionOverride();
     }
 
     if (commitScroll)
