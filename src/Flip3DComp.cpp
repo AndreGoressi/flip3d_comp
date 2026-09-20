6784dc38bbf65e0e7e8afce808abc74ac4ebbc1f
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-20 14:03:32 +0200

    Change live preview activation to use Window type

--- src/Flip3DComp.cpp
+++ src/Flip3DComp.cpp
@@ -51,7 +51,7 @@ bool Flip3DComp::Initialize(HINSTANCE hInstance)
 
     if (m_pfnActivateLivePreview)
     {
-        m_pfnActivateLivePreview(TRUE, m_hwnd, nullptr, static_cast<UINT>(PeekTypes::Desktop), nullptr); 
+        m_pfnActivateLivePreview(TRUE, m_hwnd, nullptr, static_cast<UINT>(PeekTypes::Window), nullptr); 
     }
     
     m_state = ViewState::Enter;
