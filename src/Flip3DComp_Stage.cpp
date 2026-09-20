d6a5b3bfb30180d7d4814c57bc2286a0c5610fa9
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-21 00:58:24 +0200

    Change window band setting to ZBID_SYSTEM_TOOLS

--- src/Flip3DComp_Stage.cpp
+++ src/Flip3DComp_Stage.cpp
@@ -144,12 +144,12 @@ bool Flip3DComp::InitializeDCompStage()
                                      nullptr,                                      
                                      m_hInstance,                                  
                                      this,                                         
-                                     ZBID_SYSTEM_TOOLS                                 
+                                     ZBID_UIACCESS               
     );
     if (!m_hwnd)
         return false;
 	//
-	SetWindowBand(m_hwnd, nullptr, ZBID_UIACCESS);
+	SetWindowBand(m_hwnd, nullptr, ZBID_SYSTEM_TOOLS);
 	SetTopmost(m_hwnd, TRUE);
 	//
 	m_rtl = (GetWindowLongPtrW(m_hwnd, GWL_EXSTYLE) & WS_EX_LAYOUTRTL) != 0;
