a7aab70fc337c656d9bdc641db49eb380d929c97
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-20 17:17:53 +0200

    Modify window style handling for topmost windows
    Removed WS_EX_LAYERED from the topmost window style adjustments.

--- src/Flip3DComp_ShellHook.cpp
+++ src/Flip3DComp_ShellHook.cpp
@@ -69,7 +69,7 @@ BOOL CALLBACK Flip3DComp::RemoveTopmostCallback(HWND hwnd, LPARAM lParam)
     LONG_PTR exStyle = GetWindowLongPtrW(hwnd, GWL_EXSTYLE);
     if (exStyle & WS_EX_TOPMOST)
     {
-        SetWindowLongPtrW(hwnd, GWL_EXSTYLE, (exStyle & ~WS_EX_TOPMOST) | WS_EX_LAYERED | WS_EX_TRANSPARENT);
+        SetWindowLongPtrW(hwnd, GWL_EXSTYLE, (exStyle & ~WS_EX_TOPMOST) /*| WS_EX_LAYERED*/ | WS_EX_TRANSPARENT);
         SetWindowPos(hwnd, HWND_NOTOPMOST, 0, 0, 0, 0, 
                      SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE | SWP_FRAMECHANGED);
         //
@@ -89,7 +89,7 @@ void Flip3DComp::RestoreCompetingTopmost()
         if (IsWindow(hwnd))
         {
             LONG_PTR exStyle = GetWindowLongPtrW(hwnd, GWL_EXSTYLE);
-            SetWindowLongPtrW(hwnd, GWL_EXSTYLE, (exStyle | WS_EX_TOPMOST) & ~WS_EX_LAYERED & ~WS_EX_TRANSPARENT);
+            SetWindowLongPtrW(hwnd, GWL_EXSTYLE, (exStyle | WS_EX_TOPMOST) /*& ~WS_EX_LAYERED*/ & ~WS_EX_TRANSPARENT);
             SetWindowPos(hwnd, HWND_TOPMOST, 0, 0, 0, 0, 
                          SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE | SWP_FRAMECHANGED);
         }
