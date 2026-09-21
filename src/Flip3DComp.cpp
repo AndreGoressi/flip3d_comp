9935f8fcda4b127d231c8758e3a75f7c4880c49a
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-22 00:56:38 +0200

    Update Flip3DComp.cpp

--- src/Flip3DComp.cpp
+++ src/Flip3DComp.cpp
@@ -39,7 +39,6 @@ bool Flip3DComp::Initialize(HINSTANCE hInstance)
     {
         m_pfnActivateLivePreview(TRUE, m_hwnd, nullptr, static_cast<UINT>(PeekTypes::Desktop), nullptr);
     }
-    SetTopmostDynamic(m_hwnd, TRUE);
 
     if (FAILED(InitComposition()))
     {
