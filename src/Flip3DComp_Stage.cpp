def13ca628d970d94abacd5673fd1d91f8e94423
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-21 16:45:16 +0200

    Remove SetWindowBand function and update window creation

--- src/Flip3DComp_Stage.cpp
+++ src/Flip3DComp_Stage.cpp
@@ -50,18 +50,6 @@ std::vector<HWND> Flip3DComp::EnumerateWindows()
     return ctx.hwnds;
 }
 
-BOOL Flip3DComp::SetWindowBand(HWND hWnd, HWND hwndInsertAfter, DWORD dwBand)
-{
-	if (g_iam_key)
-	{
-		m_NtUserEnableIAMAccess(g_iam_key, TRUE);
-		const auto callResult = m_SetWindowBand(hWnd, hwndInsertAfter, dwBand);
-		lSet = GetLastError();
-		m_NtUserEnableIAMAccess(g_iam_key, FALSE);
-		return callResult;
-	}
-	return FALSE;
-}
 // ============================================================================
 // Flip3DComp::ApplyFullscreenLayout
 // uDWM EnableInputHooksHelper: WS_POPUP covering m_rcVirtualScreen.
@@ -125,7 +113,7 @@ bool Flip3DComp::InitializeDCompStage()
     const int w = mi.rcWork.right - mi.rcWork.left;
     const int h = mi.rcWork.bottom - mi.rcWork.top;
 	//
-    m_hwnd = m_pfnCreateWindowInBandEx(WS_EX_NOREDIRECTIONBITMAP | 
+    m_hwnd = m_pfnCreateWindowInBand(WS_EX_NOREDIRECTIONBITMAP | 
                                      //WS_EX_TOPMOST |
                                      WS_EX_TOOLWINDOW,
                                      (LPCWSTR)res, L"",                         
@@ -135,8 +123,7 @@ bool Flip3DComp::InitializeDCompStage()
                                      nullptr,                        
                                      m_hInstance,                    
                                      this,                           
-                                     ZBID_DESKTOP,
-									 0
+                                     ZBID_DESKTOP
     );
     if (!m_hwnd)
         return false;
