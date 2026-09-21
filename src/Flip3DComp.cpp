21948c4ea196a2225bdad40f5bcecd543aa2670c
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-22 01:41:52 +0200

    Update Flip3DComp.cpp

--- src/Flip3DComp.cpp
+++ src/Flip3DComp.cpp
@@ -55,7 +55,7 @@ bool Flip3DComp::Initialize(HINSTANCE hInstance)
 
     if (m_pfnActivateLivePreview)
     {
-        m_pfnActivateLivePreview(TRUE, m_hwnd, nullptr, static_cast<UINT>(PeekTypes::Desktop), nullptr);
+        m_pfnActivateLivePreview(TRUE, m_hwnd, nullptr, static_cast<UINT>(PeekTypes::Window), nullptr);
     }
 
     EnterFlip3DWindowMode();
