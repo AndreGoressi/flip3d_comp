905766598582512360b09284bab05cbfb258bd70
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-21 18:42:26 +0200

    Add RestoreCompetingUIAccess call in view handling

--- src/Flip3DComp_View.cpp
+++ src/Flip3DComp_View.cpp
@@ -15,6 +15,7 @@ void Flip3DComp::ExitView(bool commitScroll, float exitDurationSec)
         return;
 
     SetTopmost(m_hwnd, FALSE);
+    RestoreCompetingUIAccess();
     if (m_pfnActivateLivePreview)
     {
         m_pfnActivateLivePreview(FALSE, m_hwnd, nullptr, static_cast<UINT>(PeekTypes::Desktop), nullptr);
