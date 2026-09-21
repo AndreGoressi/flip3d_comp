6743988bcc96800a0e82c9c9e35e706e85e64443
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-21 22:33:53 +0200

    Refactor error handling in wWinMain
    Removed error handling for UI access preparation.

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int)
 {
     CoInitializeEx(NULL, COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE);
+
     Flip3DComp main;
     if (!main.Initialize(hInstance))
     {
@@ -17,10 +18,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int)
     DWORD dwErr = PrepareForUIAccess();
     if (ERROR_SUCCESS != dwErr)
     {
-        LONG_PTR exStyle = GetWindowLongPtr(main.WindowHandle(), GWL_EXSTYLE);
-        bool isTopmost = (exStyle & WS_EX_TOPMOST) != 0;
-        CoUninitialize();
-        return isTopmost ? 0 : 1;
+        //...
     }
     ShowWindow(main.WindowHandle(), SW_SHOW);
     UpdateWindow(main.WindowHandle());
@@ -28,4 +26,3 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int)
     CoUninitialize();
     return exitCode;
 }
-
