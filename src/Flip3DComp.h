0ef513d37b3faf58e85ba2fd48d7246dc2580133
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-21 16:43:29 +0200

    Update Flip3DComp.h

--- src/Flip3DComp.h
+++ src/Flip3DComp.h
@@ -328,8 +328,4 @@ private:
     DwmpActivateLivePreview_fn             m_pfnActivateLivePreview = nullptr;
     SetWindowCompositionAttribute_fn m_pfnSetWindowCompositionAttribute = nullptr;
     CreateWindowInBand_fn            m_pfnCreateWindowInBand          = nullptr;
-    CreateWindowInBandEx_fn            m_pfnCreateWindowInBandEx          = nullptr;
-    SetWindowBand_fn m_SetWindowBand = nullptr;
-    GetWindowBand_fn m_GetWindowBand = nullptr;
-    NtUserEnableIAMAccess_fn m_NtUserEnableIAMAccess = nullptr;
 };
