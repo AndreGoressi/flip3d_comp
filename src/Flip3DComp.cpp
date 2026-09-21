2fbda776c28b47854114a37e7be8f35b05ab959a
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-22 00:56:22 +0200

    Remove SetTopmostDynamic function
    Removed SetTopmostDynamic function from Flip3DComp.

--- src/Flip3DComp.cpp
+++ src/Flip3DComp.cpp
@@ -121,18 +121,6 @@ HMONITOR Flip3DComp::GetTargetMonitor() const
     //
     return hPrimary;
 }
-
-bool Flip3DComp::SetTopmostDynamic(HWND hwnd, bool topmost)
-{
-    if (!hwnd) 
-        return false;
-    //
-    SetWindowPos(hwnd, topmost ? HWND_TOPMOST : HWND_NOTOPMOST,
-                 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE | SWP_ASYNCWINDOWPOS);
-                 
-    const LONG_PTR exStyle = GetWindowLongPtr(hwnd, GWL_EXSTYLE);
-    return ((exStyle & WS_EX_TOPMOST) != 0) == topmost;
-}
 // ============================================================================
 // Flip3DComp::Run
 // ============================================================================
