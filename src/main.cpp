73b1e244d883221265f2f8e92691327519ad4ad2
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-21 02:32:21 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,21 +10,21 @@ bool SetTopmost(HWND hwnd, bool topmost)
     const bool actuallyTopmost = (exStyle & WS_EX_TOPMOST) != 0;
     return actuallyTopmost == topmost;
 }
+
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int)
 {
     CoInitializeEx(NULL, COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE);
-
     Flip3DComp main;
     if (!main.Initialize(hInstance))
     {
         CoUninitialize();
         return 1;
     }
-    SetForegroundWindow(main.WindowHandle());
-    SetTopmost(main.WindowHandle(), TRUE);
-    //ShowWindow(main.WindowHandle(), SW_SHOW);
-    UpdateWindow(main.WindowHandle());
-    //
+    HWND hwnd = main.WindowHandle();
+    ShowWindow(hwnd, SW_SHOW);
+    UpdateWindow(hwnd);
+    SetTopmost(hwnd, TRUE);
+    SetForegroundWindow(hwnd);
     int result = main.Run();
     CoUninitialize();
     return result;
