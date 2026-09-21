5e63bff6a964975893928af0546dfd5c913c697b
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-21 19:07:01 +0200

    Refactor window creation to use CreateWindowInBandEx

--- src/Flip3DComp_Stage.cpp
+++ src/Flip3DComp_Stage.cpp
@@ -113,24 +113,22 @@ bool Flip3DComp::InitializeDCompStage()
     const int w = mi.rcWork.right - mi.rcWork.left;
     const int h = mi.rcWork.bottom - mi.rcWork.top;
 	//
-    m_hwnd = m_pfnCreateWindowInBand(WS_EX_NOREDIRECTIONBITMAP | 
-                                     //WS_EX_TOPMOST |
-                                     WS_EX_TOOLWINDOW,
-                                     (LPCWSTR)res, L"",                         
-                                     0x80000000,                     
-                                     x, y, w, h,                     
-                                     nullptr,                        
-                                     nullptr,                        
-                                     m_hInstance,                    
-                                     this,                           
-                                     ZBID_DESKTOP
+    m_hwnd = m_pfnCreateWindowInBandEx(WS_EX_NOREDIRECTIONBITMAP | 
+                                       //WS_EX_TOPMOST |
+                                       WS_EX_TOOLWINDOW,
+                                       (LPCWSTR)res, 
+								       L"",                         
+                                       0x80000000,                     
+                                       x, y, w, h,                     
+                                       nullptr,                        
+                                       nullptr,                        
+                                       m_hInstance,                    
+                                       this,                           
+                                       ZBID_DESKTOP,
+									   0
     );
-	SetTopmost(m_hwnd, TRUE);
     if (!m_hwnd)
-	{
-		SetTopmost(m_hwnd, FALSE);
         return false;
-	}
     //
     m_rtl = (GetWindowLongPtrW(m_hwnd, GWL_EXSTYLE) & WS_EX_LAYOUTRTL) != 0;
     //
