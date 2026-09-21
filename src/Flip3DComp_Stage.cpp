b3164b014d9bb6fcb0fc25206c00203ee6ddb68a
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-21 15:19:01 +0200

    Replace window creation function with pfnCreateWindowInBandEx

--- src/Flip3DComp_Stage.cpp
+++ src/Flip3DComp_Stage.cpp
@@ -125,18 +125,19 @@ bool Flip3DComp::InitializeDCompStage()
     const int w = mi.rcWork.right - mi.rcWork.left;
     const int h = mi.rcWork.bottom - mi.rcWork.top;
     //
-    m_hwnd = m_pfnCreateWindowInBand(WS_EX_NOREDIRECTIONBITMAP | 
-                                     //WS_EX_TOPMOST |
-                                     WS_EX_TOOLWINDOW,
-                                     (LPCWSTR)res, L"",                         
-                                     0x80000000,                     
-                                     x, y, w, h,                     
-                                     nullptr,                        
-                                     nullptr,                        
-                                     m_hInstance,                    
-                                     this,                           
-                                     ZBID_DESKTOP
-    );
+	m_hwnd = pfnCreateWindowInBandEx(WS_EX_NOREDIRECTIONBITMAP | 
+									 WS_EX_TOOLWINDOW,
+	    							 (LPCWSTR)res, 
+									 L"",
+	    							 0x80000000,
+	    						     x, y, w, h,
+	    							 nullptr, 
+	    							 nullptr,
+	    							 m_hInstance,
+	    							 this,
+	    							 ZBID_DESKTOP,
+	    							 0 // dwTypeFlags
+	);
     if (!m_hwnd)
         return false;
     //
