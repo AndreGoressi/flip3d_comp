0136479c2e45995d76b1cc0de4ad9fdea8456540
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-22 00:57:42 +0200

    Update Flip3DComp_Stage.cpp

--- src/Flip3DComp_Stage.cpp
+++ src/Flip3DComp_Stage.cpp
@@ -139,7 +139,9 @@ bool Flip3DComp::InitializeDCompStage()
                                        ZBID_DESKTOP,
 									   0
     );
+	SetTopmostDynamic(m_hwnd, true);
     if (!m_hwnd)
+		    SetTopmostDynamic(m_hwnd, false);
         return false;
     //
     m_rtl = (GetWindowLongPtrW(m_hwnd, GWL_EXSTYLE) & WS_EX_LAYOUTRTL) != 0;
