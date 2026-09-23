aa4dad60d35552a304fefcfa49639793d333b70f
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-23 14:18:47 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #include "PrepareForUIAccess.h"
 //
 
-bool SetAlwaysOnTop(HWND hwnd, bool topmost)
+bool SetAlwaysOnTopAsync(HWND hwnd, bool topmost)
 {
     if (!hwnd) 
         return false;
@@ -34,7 +34,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int)
         //...
     }
     ShowWindow(main.WindowHandle(), SW_SHOW);
-    SetAlwaysOnTop(main.WindowHandle(), true);
+    SetAlwaysOnTopAsync(main.WindowHandle(), true);
     UpdateWindow(main.WindowHandle());
     int exitCode = main.Run();
     CoUninitialize();
