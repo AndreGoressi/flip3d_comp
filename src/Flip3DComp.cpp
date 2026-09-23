947b28e60e2f5433258a79ba1d10050df1485ead
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-23 14:21:38 +0200

    Update Flip3DComp.cpp

--- src/Flip3DComp.cpp
+++ src/Flip3DComp.cpp
@@ -56,7 +56,7 @@ bool Flip3DComp::Initialize(HINSTANCE hInstance)
     EnterFlip3DWindowMode();
     InitAccessibility(); 
 
-    SetAeroPeekEnabled(true, /*delayed=*/true, /*delayMs=*/100);
+    SetPeekLivePreview(true, /*delayed=*/true, /*delayMs=*/100);
     
     Update(0.0f);
     
