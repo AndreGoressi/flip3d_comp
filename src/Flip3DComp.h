fb0f28e460b860468378cbc247fcc2a38434c241
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-23 17:52:35 +0200

    Change default delayMs to 0 in SetPeekLivePreview

--- src/Flip3DComp.h
+++ src/Flip3DComp.h
@@ -247,7 +247,7 @@ private:
     BOOL    SetWindowBand(HWND hWnd, HWND hwndInsertAfter, DWORD dwBand);
     BOOL GetWindowBand(HWND hWnd, DWORD* pdwBand);
     //
-    void SetPeekLivePreview(bool enable, bool delayed = false, UINT delayMs = 50);
+    void SetPeekLivePreview(bool enable, bool delayed = false, UINT delayMs = 0);
     //
     // ========================================================================
     // Member variables
