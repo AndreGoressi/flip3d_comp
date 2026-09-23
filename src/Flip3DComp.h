f5283ad247d4fc6f1988b11f7ba89bd54740c453
AndreGoressi <143658621+AndreGoressi@users.noreply.github.com>
2026-09-23 14:19:15 +0200

    Change default delayMs in SetAeroPeekEnabled to 50

--- src/Flip3DComp.h
+++ src/Flip3DComp.h
@@ -247,7 +247,7 @@ private:
     BOOL    SetWindowBand(HWND hWnd, HWND hwndInsertAfter, DWORD dwBand);
     BOOL GetWindowBand(HWND hWnd, DWORD* pdwBand);
     //
-    void SetAeroPeekEnabled(bool enable, bool delayed = false, UINT delayMs = 0);
+    void SetAeroPeekEnabled(bool enable, bool delayed = false, UINT delayMs = 50);
     //
     // ========================================================================
     // Member variables
